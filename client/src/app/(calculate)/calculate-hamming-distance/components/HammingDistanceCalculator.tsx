2690221b24db84c41e958489ead57cb3598ccfb7
pl3lee <billylee123@gmail.com>
2023-08-09 17:25:13 +0800

    removed name prop from matrix

---
+++ client/src/app/(calculate)/calculate-hamming-distance/components/HammingDistanceCalculator.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Calculator from "@/components/Calculator";
+import { useState, useEffect } from "react";
+
+const HammingDistanceCalculator = () => {
+  const [words, setWords] = useState<number[][]>;
+  return <Calculator />;
+};
+
+export default HammingDistanceCalculator;
