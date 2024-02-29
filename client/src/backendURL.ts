1d3208ae4628ffa4133627bf7932efecfba74381
pl3lee <billy.pl.lee@gmail.com>
2024-02-29 01:45:56 +0000

    changed backend url to digital ocean host

--- client/src/backendURL.ts
+++ client/src/backendURL.ts
@@ -1,1 +1,1 @@
-export const backendURL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "http://138.197.146.67:3001"
+export const backendURL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://calculator-backend-do.billylee.me"
